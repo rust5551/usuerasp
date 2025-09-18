93950ff9778efb8f4548aa6fdf02b97be1eb7a69
rust5551 <rustamkliz018@gmail.com>
2025-09-18 17:47:23 +0500

    finally good docker lol

--- bot.py
+++ bot.py
@@ -6,9 +6,11 @@ from datetime import datetime, timedelta
 from schedule import make_it_pretty
 # from database import add_user, change_group
 
-def logger(update):
-    with open("logs.txt", "a+", encoding="UTF-8") as f:
-        f.write(f"[{update.effective_user.first_name} | @{update.effective_user.username} | {update.message.date.astimezone()}]   {update.message.text} \n ——————————————— \n")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+# def logger(update):
+#     with open("logs.txt", "a+", encoding="UTF-8") as f:
+#         f.write(f"[{update.effective_user.first_name} | @{update.effective_user.username} | {update.message.date.astimezone()}]   {update.message.text} \n ——————————————— \n")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
@@ -20,12 +22,12 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         await texts[update.effective_message.text](update, context)
     except:
-        logger(update)
+        # logger(update)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Введите /start")
     #await context.bot.send_message(chat_id=update.effective_chat.id, text=update.effective_message.text)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger(update)
+    # logger(update)
     reply_keyboard = [["На неделю", "На завтра", "На сегодня"]]
 
     await update.message.reply_text("Hello there", reply_markup=ReplyKeyboardMarkup(
@@ -33,7 +35,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ))
 
 async def rasp(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger(update)
+    # logger(update)
     start_date = datetime.today()
     end_date = start_date + timedelta(days=6)
     if context.args:
@@ -47,13 +49,13 @@ async def rasp(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=make_it_pretty(start_date, end_date, "ИВТ-24-1"), parse_mode="HTML")
 
 async def rasptd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger(update)
+    # logger(update)
     start_date = datetime.today()
     end_date = start_date
     await context.bot.send_message(chat_id=update.effective_chat.id, text=make_it_pretty(start_date, end_date, "ИВТ-24-1"), parse_mode="HTML")
 
 async def rasptm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger(update)
+    # logger(update)
     start_date = datetime.today() + timedelta(days=1)
     end_date = start_date
     await context.bot.send_message(chat_id=update.effective_chat.id, text=make_it_pretty(start_date, end_date, "ИВТ-24-1"), parse_mode="HTML")
